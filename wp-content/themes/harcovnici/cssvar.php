718a158dccde9ae8b658a9b243e6551287a839b3
Ales Krejci <krejci@ales.net>
2018-10-13 20:41:49 +0200

    Puvodni verze sablony

---
+++ wp-content/themes/harcovnici/cssvar.php
@@ -0,0 +1,11 @@
+<?php
+if (in_array(date("n"), ["1", "2", "3", "4"])) {
+    echo "<link rel=\"stylesheet\" href=\"cssvar1.css\" type=\"text/css\" charset=\"utf-8\">";
+}
+if (in_array(date("n"), ["5", "6", "7", "8"])) {
+    echo "<link rel=\"stylesheet\" href=\"cssvar2.css\" type=\"text/css\" charset=\"utf-8\">";
+}
+if (in_array(date("n"), ["9", "10", "11", "12"])) {
+    echo "<link rel=\"stylesheet\" href=\"cssvar3.css\" type=\"text/css\" charset=\"utf-8\">";
+}
+?>
