849772ada156bfe63e36dd449bcfa4796648dfc2
Ales Krejci <krejci@ales.net>
2021-02-20 18:38:36 +0100

    Odstraneni balastu (jQuery)

--- wp-content/themes/harcovnici/functions.php
+++ wp-content/themes/harcovnici/functions.php
@@ -13,37 +13,6 @@ function blankslate_setup()
   }
   register_nav_menus(array('main-menu' => esc_html__('Main Menu', 'blankslate')));
 }
-add_action('wp_enqueue_scripts', 'blankslate_load_scripts');
-function blankslate_load_scripts()
-{
-  wp_enqueue_script('jquery');
-}
-add_action('wp_footer', 'blankslate_footer_scripts');
-function blankslate_footer_scripts()
-{
-?>
-<script>
-jQuery(document).ready(function($) {
-  var deviceAgent = navigator.userAgent.toLowerCase();
-  if (deviceAgent.match(/(iphone|ipod|ipad)/)) {
-    $("html").addClass("ios");
-    $("html").addClass("mobile");
-  }
-  if (navigator.userAgent.search("MSIE") >= 0) {
-    $("html").addClass("ie");
-  } else if (navigator.userAgent.search("Chrome") >= 0) {
-    $("html").addClass("chrome");
-  } else if (navigator.userAgent.search("Firefox") >= 0) {
-    $("html").addClass("firefox");
-  } else if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
-    $("html").addClass("safari");
-  } else if (navigator.userAgent.search("Opera") >= 0) {
-    $("html").addClass("opera");
-  }
-});
-</script>
-<?php
-}
 add_filter('document_title_separator', 'blankslate_document_title_separator');
 function blankslate_document_title_separator($sep)
 {
